153fd019c512f631b0301175cba2b760fc62bd58
green <Alexey.Kamenew@yandex.ru>
2026-01-13 20:17:04 +0300

    feat: use unchecked utf conversion

--- src/utfutils.cpp
+++ src/utfutils.cpp
@@ -9,14 +9,14 @@ namespace c12cxx {
 std::string toUtf8(std::u16string_view utf16_sv)
 {
     std::string utf8_str;
-    utf8::utf16to8(utf16_sv.begin(), utf16_sv.end(), std::back_inserter(utf8_str));
+    utf8::unchecked::utf16to8(utf16_sv.begin(), utf16_sv.end(), std::back_inserter(utf8_str));
     return utf8_str;
 }
 
 std::u16string toUtf16(std::string_view utf8_sv)
 {
     std::u16string utf16_str;
-    utf8::utf8to16(utf8_sv.begin(), utf8_sv.end(), std::back_inserter(utf16_str));
+    utf8::unchecked::utf8to16(utf8_sv.begin(), utf8_sv.end(), std::back_inserter(utf16_str));
     return utf16_str;
 }
 
